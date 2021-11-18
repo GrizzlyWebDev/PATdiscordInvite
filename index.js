f3b6fa49fb5a1a9379c64029dcaeaff672e03463
Phill Blake <grizzlyintent@yahoo.com>
2021-11-17 19:31:06 -0500

    created server restart

--- index.js
+++ index.js
@@ -6,25 +6,38 @@ const { token } = require('./config.json');
 let invite;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, 'GUILD_MEMBERS', 'GUILD_INVITES', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] });
 client.on('ready', async () => {
-	const guild = client.guilds.cache.find(g => g.name === "Test");
-	if(!guild || !guild.available) return message.channel.send("Can't find guild");
-	const channel = guild.channels.cache.find(c => c.name === "general");
-	invite = await channel.createInvite({
-		maxUses: 1,
-		temporary: true,
+		const guild = client.guilds.cache.find(g => g.name === "Test");
+		if(!guild || !guild.available) return message.channel.send("Can't find guild");
+		const channel = guild.channels.cache.find(c => c.name === "general");
+		invite = await channel.createInvite({
+			maxUses: 1,
+			temporary: true,
+		})
 	})
-})
-
-client.login(token);
+	
+	client.login(token);
 
 app.get('/', async (req, res) => {
+	
 	
 	 if (invite.code === "undefined") {
 	 	res.status(404).send('invite is not available')
 	 } else {
 		res.send('https://discord.gg/' + invite.code);
 	 }
+	 setTimeout(function () {
+        // When NodeJS exits
+        process.on("exit", function () {
+
+            require("child_process").spawn(process.argv.shift(), process.argv, {
+                cwd: process.cwd(),
+                detached : true,
+                stdio: "inherit"
+            });
+        });
+        process.exit(1);
+    }, 1000);
 });
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 4000; 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
