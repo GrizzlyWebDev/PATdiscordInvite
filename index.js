0b809138f3350a03765133f71bdcf53fc0490003
Phill Blake <grizzlyintent@yahoo.com>
2021-11-20 11:33:59 -0500

    added /open and /close

--- index.js
+++ index.js
@@ -1,51 +1,80 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Client, Intents} = require('discord.js');
-const { token } = require('./config.json');
-const cors = require('cors');
-let invite;
+const { Client, Intents } = require("discord.js");
+const { token } = require("./config.json");
+const cors = require("cors");
+let invite = "Server is closed";
+let open = false;
 
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    "GUILD_MEMBERS",
+    "GUILD_INVITES",
+    "GUILD_MESSAGES",
+    "GUILD_MESSAGE_REACTIONS",
+  ],
+});
+
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(invite);
+});
+client.on("messageCreate", async (msg) => {
+  if (msg.content === "/open") {
+    invite = await msg.channel.createInvite({
+      maxUses: 1,
+      temporary: true,
+    });
+	invite = "https://discord.gg/" + invite.code;
+    open = true;
+  }
+});
+client.on("messageCreate", async (msg) => {
+	if (msg.content === "/closed") {
+	  
+	  invite = "Server is closed";
+	  open = false;
+	}
   });
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, 'GUILD_MEMBERS', 'GUILD_INVITES', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] });
-client.on('ready', async () => {
-		const guild = client.guilds.cache.find(g => g.name === "Test");
-		if(!guild || !guild.available) return message.channel.send("Can't find guild");
-		const channel = guild.channels.cache.find(c => c.name === "general");
-		invite = await channel.createInvite({
-			maxUses: 1,
-			temporary: true,
-		})
-	})
-	client.on('guildMemberAdd', async () => {
-		const channel = client.channels.cache.find(c => c.name === "general");
-		invite = await channel.createInvite({
-			maxUses: 1,
-			temporary: true,
-		})
-	}) 
-		
-	client.login(token);
-
-app.get('/', async (req, res) => {
-	
-	
-	 if (invite.code === "undefined") {
-	 	res.status(404).send('invite is not available')
-	 } else {
-		res.send('https://discord.gg/' + invite.code);
-	 }
-	
+client.on("guildMemberAdd", async () => {
+  if (open === "true") {
+    const channel = client.channels.cache.find((c) => c.name === "general");
+    invite = await channel.createInvite({
+      maxUses: 1,
+      temporary: true,
+    });
+	invite = "https://discord.gg/" + invite.code;
+  } else {
+	  invite = "Server is closed";
+  }
 });
-app.use(cors({
-    origin: 'https://pat-thenticator.netlify.app',
-	methods: ['GET']
-}));
 
-const port = process.env.PORT || 3000; 
+client.login(token);
+
+app.get("/", async (req, res) => {
+  if (invite.code === "undefined") {
+    res.status(404).send("invite is not available");
+  } else {
+    res.send(invite);
+  }
+});
+app.use(
+  cors({
+    origin: "https://pat-thenticator.netlify.app",
+    methods: ["GET"],
+  })
+);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
